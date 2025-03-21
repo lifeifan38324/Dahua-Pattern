244d8a0e8456fc33b05166862c8bd81ccc60cc54
YiMojin <yimojin@work.com>
2025-03-21 13:59:23 +0800

    use cmd to run make in tasks.json, default terminal is PowerShell. add pause function in main.cpp.

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,18 @@
 #include<iostream>
 #include<stdio.h>
 #include<string>
-// #include"memento_pattern/include2.h"
+#include"memento_pattern/include2.h"
 using namespace std;
 
+void my_pause(){
+	cout << "Press any key to continue . . .";
+	string s;
+	getline(cin, s);
+}
+
 int main(int argc, char *argv[]){
-	// include_main();
-	cout << "liff" << endl;
+	include_main();
 	
-	system("pause");
+	my_pause();
 	return 0;
 }
